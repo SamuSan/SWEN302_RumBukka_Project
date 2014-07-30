f8913a91b667269dd8997a485499ae33316f82af
OvGenocide <j.watkins.nz@gmail.com>
2014-07-31 10:55:38 +1200

    changed the controller for reals

--- Web/Controllers/addPersonController.js
+++ Web/Controllers/addPersonController.js
@@ -1,5 +1,18 @@
  var rumBukkaApp = angular.module('rumBukkaApp',['ngRoute']);
 
+rumBukkaApp.controller('addPersonController', function ($scope, $route, $location, $window, $rootScope) {
  var departs = ["Engineering and Computer Science", "Science", "Maths", "Bachelor of arts/working at mcdonalds"];
-
  $scope.departs = departs;
+  $scope.newUser = {
+        "firstName": "",
+        "lastName": "",
+        "SID": "",
+        "startDate":"",
+        "endDate": "",
+        "department": ""
+    };
+
+    //submit function
+    
+
+}
