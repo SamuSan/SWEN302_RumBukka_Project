9f792f098299062ab938ff3a68320ac2521db879
Crystal Johnston <crystalvjohnston@gmail.com>
2014-07-31 13:18:02 +1200

    submit stuff for add person

--- Web/Controllers/addPersonController.js
+++ Web/Controllers/addPersonController.js
@@ -4,15 +4,39 @@ rumBukkaApp.controller('addPersonController', function ($scope, $route, $locatio
  var departs = ["Engineering and Computer Science", "Science", "Maths", "Bachelor of arts/working at mcdonalds"];
  $scope.departs = departs;
   $scope.newUser = {
-        "firstName": "",
-        "lastName": "",
-        "SID": "",
+        "FirstName": "",
+        "LastName": "",
+        "VUWStudentId": "",
+        "Type": "",
+        "Phone_Phone_Id": "",
+        "Organisation_Organisation_Id": "",
         "startDate":"",
         "endDate": "",
-        "department": ""
     };
 
     //submit function
+    $scope.submit = function(){
+    	var newUser = $scope.newUser;
+    	$scope.addUser = {
+        "FirstName": "",
+        "LastName": "",
+        "VUWStudentId": "",
+        "Type": "",
+        "Phone_Phone_Id": "",
+        "Organisation_Organisation_Id": "",
+        "startDate":"",
+        "endDate": "",
+    	};
+    	var addUser =$scope.addUser;
+    	addUser.FirstName = newUser.FirstName;
+    	addUser.LastName = newUser.LastName;
+    	addUser.VUWStudentId = newUser.VUWStudentId;
+    	addUser.Type = newUser.Type;
+    	addUser.Phone_Phone_Id = newUser.Phone_Phone_Id;
+    	addUser.Organisation_Organisation_Id = newUser.Organisation_Organisation_Id;
+    	addUser.startDate = newUser.startDate;
+    	addUser.endDate = newUser.endDate;
+    }
     
 
 }
