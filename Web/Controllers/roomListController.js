529d415dda2786891e684ea5c54be53b33b0567b
SamuSan <sambminns@gmail.com>
2014-10-09 13:40:12 +1300

    Timeline room selection fix

--- Web/Controllers/roomListController.js
+++ Web/Controllers/roomListController.js
@@ -1,4 +1,5 @@
-rumBukkaApp.controller('roomListController', function($scope, $resource, $route, $location, $window, $rootScope, roomData) {
+rumBukkaApp.controller('roomListController', function($scope, $resource, $route, $location, $window, $rootScope, roomData, bookingData) {
+  $scope.bookings = [];
   roomData.getRooms().$promise.then(function(rooms) {
 
     $scope.Rooms = rooms;
@@ -7,13 +8,30 @@ rumBukkaApp.controller('roomListController', function($scope, $resource, $route,
   //submit function
   $scope.submit = function() {
 
-    if ($scope.selectedRoom != null) {
-      $location.url('/roomTimeline');
-    }
+    bookingData.getBookings().$promise.then(function(bookings) {
+
+      console.log(bookings);
+      for (var i = bookings.length - 1; i >= 0; i--) {
+        if (bookings[i].Room.RoomName == $scope.selectedRoom) {
+          $scope.bookings.push(bookings[i]);
+        }
+
+      };
+      if ($scope.bookings.length == 0) {
+        alert("No bookings for selected room");
+        $location.url('/roomSummary');
+      } else if ($scope.selectedRoom != null) {
+        $location.url('/roomTimeline/' + $scope.selectedRoom);
+      }
+
+    });
+
+
+
   };
-$scope.cancel = function(){
-   $location.url('/');
-}
+  $scope.cancel = function() {
+    $location.url('/');
+  }
 
   $scope.selectedRoom = null;
   $scope.setSelected = function(selectedRoom) {
